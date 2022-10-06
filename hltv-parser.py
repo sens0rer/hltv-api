fb85ff30c5acd91abdc24f6369cf41d34e3af5c0
Artem Omelchenko <56344591+sens0rer@users.noreply.github.com>
2022-10-06 22:47:41 +0300

    Started implementing getting match stats

--- hltv-parser.py
+++ hltv-parser.py
@@ -5,12 +5,16 @@ import requests
 from bs4 import BeautifulSoup
 
 def getTeamInfo(teamid, teamname):
+    """
+    Returns a dictionary of basic team stats
+    """
     teamInfo = {}
     teamInfo['Team ID'] = teamid
     teamInfo['Team name'] = teamname.lower()
     page = requests.get('https://www.hltv.org/team/' + str(teamid) + '/' + teamname.lower())
     htmlstr = page.text
     
+    # extract World ranking
     start = htmlstr.find('<div class="profile-team-stat"><b>World ranking</b>')
     ranking = htmlstr[start:]
     start = ranking.find('#')
@@ -18,12 +22,14 @@ def getTeamInfo(teamid, teamname):
     ranking = int(ranking[start+1:end])
     teamInfo['World ranking'] = ranking
     
+    # Extract amount of weeks in top30 for core
     start = htmlstr.find('<div class="profile-team-stat"><b>Weeks in top30 for core</b>')
     weeksInTop30 = htmlstr[start+81:]
     end = weeksInTop30.find("</span>")
     weeksInTop30 = int(weeksInTop30[0:end])
     teamInfo['Weeks in top30 for core'] = weeksInTop30
     
+    # Extract coach nickname and id
     start = htmlstr.find('<div class="profile-team-stat"><b>Coach</b>')
     if start != -1:
         coach = htmlstr[start+60:]
@@ -36,6 +42,7 @@ def getTeamInfo(teamid, teamname):
         teamInfo['Coach'] = {'Name': None,
                              'ID': None}
     
+    # Extract player nicknames and ids
     players_raw = hltv.get_players(teamid)
     players = []
     for player in players_raw:
@@ -47,6 +54,10 @@ def getTeamInfo(teamid, teamname):
     return teamInfo
 
 def _extractPlayerStats(htmlstr, prompt):
+    """
+    Extracts required player stat(prompt) from player's page html code converted
+    to string(htmlstr)
+    """
     start = htmlstr.find(prompt)
     htmlstr = htmlstr[start:]
     start = htmlstr.find('Val')
@@ -56,6 +67,9 @@ def _extractPlayerStats(htmlstr, prompt):
 
 
 def getPlayerInfo(playerid, nickname):
+    """
+    Returns a dictionary of player stats
+    """
     playerInfo = {}
     playerInfo['ID'] = playerid
     playerInfo['Name'] = nickname.lower()
@@ -71,7 +85,69 @@ def getPlayerInfo(playerid, nickname):
 
     return playerInfo
 
-def getMatchInfo(url):
+def getFinishedMatchInfo(url):
+    """
+    Returns a dictionary of match stats
+    """
+    matchInfo = {}
+    matchInfo['URL'] = url
+    page = requests.get(url)
+    htmlstr = page.text
+    
+    # Team names and ids
+    start = htmlstr.find('team1-gradient')
+    teams = htmlstr[start+31:]
+    end = teams.find('">')
+    matchInfo['Team 1'] = {'Name': teams[0:end].split("/")[1],
+                           'ID': teams[0:end].split("/")[0]}
+    
+    start = teams.find('team2-gradient')
+    teams = teams[start+31:]
+    end = teams.find('">')
+    matchInfo['Team 2'] = {'Name': teams[0:end].split("/")[1],
+                           'ID': teams[0:end].split("/")[0]}
+    
+    # Best of ?
+    start = htmlstr.find('Best of')
+    boN = htmlstr[start+8:]
+    end = boN.find(" ")
+    boN = boN[0:end]
+    matchInfo['bo?'] = int(boN)
+    
+    # Result
+    start = htmlstr.find('teamName')
+    score = htmlstr[start:]
+    start = score.find('</div>')
+    score = score[start:]
+    start = score.find('<div class="score">')
+    score = score[start:]
+    start = score.find('spoiler')
+    score = score[start+9:]
+    end = score.find("<")
+    scoreT1 = score[0:end]
+    start = score.find('spoiler')
+    score = score[start+9:]
+    end = score.find("<")
+    scoreT2 = score[0:end]
+    if matchInfo['bo?'] > 1:
+        matchInfo['Team 1 points'] = int(scoreT1)
+        matchInfo['Team 2 points'] = int(scoreT2)
+    else:
+        if scoreT1 == scoreT2:
+            matchInfo['Team 1 points'] = 0.5
+            matchInfo['Team 2 points'] = 0.5
+        elif scoreT1 > scoreT2:
+            matchInfo['Team 1 points'] = 1
+            matchInfo['Team 2 points'] = 0
+        else:
+            matchInfo['Team 1 points'] = 0
+            matchInfo['Team 2 points'] = 1
+    
+    print(matchInfo)
+    return htmlstr
+
+def getUpcomingMatchInfo(url):
     pass
 
-matches = hltv.get_matches()
+# matches = hltv.get_matches()
+mInfo = getFinishedMatchInfo('https://www.hltv.org/matches/2358424/faze-vs-sprout-iem-road-to-rio-2022-europe-rmr-a')
